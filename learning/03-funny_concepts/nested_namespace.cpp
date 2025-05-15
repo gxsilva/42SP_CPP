3d4ad91d690d409d1a661fedfa8e05e478ae4620
gxsilva <lssvgabriel@gmail.com>
2025-05-14 22:51:41 -0300

    learn about reference and deference

--- learning/03-funny_concepts/nested_namespace.cpp
+++ learning/03-funny_concepts/nested_namespace.cpp
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 21:41:33 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/05/14 21:45:19 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/05/14 22:22:16 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,31 @@ namespace lsilva
 	{
 		void sayHello(void)
 		{
-			std::cout << "Hello";
+			std::cout << "Hello" << "\n";
 		}
 	}
 	void	sayBye(void)
 	{
-		std::cout << "bye";
+		std::cout << "bye" << "\n";
 	}
+}
+
+/*
+this function are not operator overloading,
+the namespace ensure a separete scope for 
+the "same" name funcion
+*/
+
+void	sayBye(void)
+{
+	std::cout << "A global bye" << "\n";
 }
 
 int main(void)
 {
 	lsilva::sayBye();
 	lsilva::functions::sayHello();
+
+	sayBye();
+	return (0);
 }
---
+++ learning/03-funny_concepts/references.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   references.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 22:29:54 by lsilva-x          #+#    #+#             */
+/*   Updated: 2025/05/14 22:49:02 by lsilva-x         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+/*
+	Reference and Deference in CPP
+	
+	Reference (variable it self)
+	References in C is an alias for an existing variable, it does not
+	hold its own value but referes directly to the memory of another variable
+
+	Deference (address of the variable)
+	Access the value stored at the address a pointer holds, it's done using the *
+	operator;
+
+	+------+     +--------+
+	|  a   |<--> |  ref   |   // same memory
+	+------+     +--------+
+		|
+		v
+		+----------+
+		|   ptr    | ---> [address of a]
+		+----------+
+
+*/
+int main(void)
+{
+	/*Reference*/
+	int		x = 10;
+	int&	x_ref = x;
+
+	int		z = 35;
+	int&	z_ref = z;
+
+	int		y = 20;
+	int*	y_def = &y;
+
+	std::cout << "X straight value: " << x << "\n";
+	std::cout << "X straight ptr value: " << &x << "\n";
+	std::cout << "X value acessed from it reference: " << x_ref << "\n";
+	
+	std::cout << "Y straight value: " << y_def << "\n";
+	std::cout << "Y acesses the ptr value: " << *y_def << "\n";
+	std::cout << "Y value acessed from it reference: " << *y_def << "\n";
+	
+	std::cout << "\n\n";
+	std::cout << "Z_ref ptr" << &z_ref << "\n";
+	std::cout << "Z_ref val" << z_ref << "\n";
+	z_ref = x;
+	std::cout << "Z_ref ptr" << &z_ref << "\n";
+	std::cout << "Z_ref val" << z_ref << "\n";
+	std::cout << "Z_ref val" << z << "\n";
+	std::cout << "\n\n";
+	
+
+	std::cout << "Y straight value: " << y_def << "\n";
+	std::cout << "Y acesses the ptr value: " << *y_def << "\n";
+	std::cout << "Y value acessed from it reference: " << *y_def << "\n";
+	return (0);
+}
