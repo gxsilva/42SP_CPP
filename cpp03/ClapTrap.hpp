ee6fb0354b872accb0b07405ab2351a36b757e31
Lucas <gxsilva>
2025-05-26 01:38:41 -0300

    cpp03

---
+++ cpp03/ClapTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 01:37:59 by lsilva-x          #+#    #+#             */
+/*   Updated: 2025/05/26 01:38:11 by lsilva-x         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ClapTrap_H
+#define ClapTrap_H
+
+class ClapTrap
+{
+private:
+	/* data */
+public:
+	ClapTrap(/* args */);
+	~ClapTrap();
+};
+
+ClapTrap::ClapTrap(/* args */)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+
+#endif // ClapTrap_H
